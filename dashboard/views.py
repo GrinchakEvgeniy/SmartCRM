afc45612debbf2bca0209199b832d7bb0750aece
GrinchakEvgeniy <evgeniy.smartpipl@gmail.com>
2021-02-04 14:54:20 +0200

    add login

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def main(request):
-    return render(request, 'dashboard/index.html')
+    return render(request, 'dashboard/index.html', {'is_login': request.user.is_authenticated})
+
