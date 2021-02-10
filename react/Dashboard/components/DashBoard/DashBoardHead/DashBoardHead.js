49ff2a494ed9f5c158c3353a3728afe74ead9adc
romazpua <roma.zp.ua@gmail.com>
2021-02-10 14:47:09 +0200

    add slide dashboardhead

--- react/Dashboard/components/DashBoard/DashBoardHead/DashBoardHead.js
+++ react/Dashboard/components/DashBoard/DashBoardHead/DashBoardHead.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './DashBoardHead.scss'
 
 const DashBoardHead = () => {
+
+    const [showMenu, setShowMenu] = useState(false)
+    const [headWidth, setHeadWidth] = useState(0)
+    const [active, setActive] = useState('')
+    const selectWidth = () => {
+
+        if(showMenu){
+            setHeadWidth(0)
+            setActive('')
+        } else {
+            setHeadWidth(200)
+            setActive(' active')
+        }
+    }
+
     return (
-        <div className="dashboard__head">
-            <div className="burgerMenu">
-                <div className="line__wrap">
+        <div className={"dashboard__head_wrap" + active}
+             style={{width: "calc(100% - " + headWidth + "px)"}}>
+            <div className="dashboard__head">
+                <div className="burgerMenu"
+                     onClick={() => {
+                         setShowMenu(!showMenu)
+                         selectWidth()
+                     }}>
                     <span/>
                     <span/>
                     <span/>
                 </div>
-            </div>
-            <div className="logo">
-                <img src="/static/images/logo.png" alt="logo"/>
-            </div>
-            <div className="userMenu">
-                <h3>hello user</h3>
-                <div className="userAva">
-                    <img src="/static/images/userIcon.svg" alt="ava"/>
+                <div className="logo">
+                    <img src="/static/images/logo.png" alt="logo"/>
+                </div>
+                <div className="userMenu">
+                    <h3>hello user</h3>
+                    <div className="userAva">
+                        <img src="/static/images/userIcon.svg" alt="ava"/>
+                    </div>
                 </div>
             </div>
         </div>
